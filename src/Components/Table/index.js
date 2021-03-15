0a479d7d563f5d7719b7e7a1446a867b9b9f08b8
chavalk <chavalk@hotmail.com>
2021-03-15 16:23:34 -0500

    Change code to pull information from state to add to table.

--- src/Components/Table/index.js
+++ src/Components/Table/index.js
@@ -17,30 +17,21 @@ class Table extends Component {
             <table className="table">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Country</th>
+                        <th scope="col">Age</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
+                    {this.state.employees.map(result => (
+                        <tr>
+                            <td>{result.name.first}</td>
+                            <td>{result.name.last}</td>
+                            <td>{result.location.country}</td>
+                            <td>{result.dob.age}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         );
