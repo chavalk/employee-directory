7bf68d54bbabb5b35309baa520ee07a8795fd21d
chavalk <chavalk@hotmail.com>
2021-03-15 15:36:03 -0500

    Change class to className for attributes in jumbotron's index.js.

--- src/Components/Jumbotron/index.js
+++ src/Components/Jumbotron/index.js
@@ -3,9 +3,9 @@ import "./style.css";
 
 function Jumbotron() {
     return (
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Employee Directory</h1>
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Employee Directory</h1>
             </div>
         </div>
     );
