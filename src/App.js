e49c698f81c49da9c2c8a4285aab91304682671e
chavalk <chavalk@hotmail.com>
2021-03-16 01:02:25 -0500

    Add button folder and index.js to sort table by first name. Add component to app.js.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Jumbotron from "./Components/Jumbotron";
 import Table from "./Components/Table";
+import SortButton from "./Components/SortButton";
 
 function App() {
   return (
     <div>
       <Jumbotron />
+      <SortButton />
       <Table />
     </div>
   );
---
+++ src/Components/SortButton/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SortButton() {
+    return (
+        <button type="button" className="btn btn-dark ml-3 mb-4">Sort By First Name</button>
+    )
+}
+
+export default SortButton;
