c51a01d407a321e5888f83a07b47c1089d19d1fd
chavalk <chavalk@hotmail.com>
2021-03-16 17:45:18 -0500

    Delete sort button folder since the button was included in table folder in index.js.

--- src/Components/SortButton/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function SortButton() {
-    return (
-        <button type="button" className="btn btn-dark ml-3 mb-4">Sort By First Name</button>
-    )
-}
-
-export default SortButton;
